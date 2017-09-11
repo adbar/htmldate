d8fdd2bf138bb99661a9542989d75d78542b0c8b
Adrien Barbaresi <adrien.barbaresi@oeaw.ac.at>
2017-09-11 17:00:14 +0200

    more tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -14,16 +14,29 @@ logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
 
 
 MOCK_PAGES = { \
-                'http://blog.python.org/2016/12/python-360-is-now-available.html': 'cache/blog.python.org.html', \
-                'https://example.com': 'cache/example.com.html', \
-                'https://github.com/adbar/htmldate': 'cache/github.com.html', \
-                'https://creativecommons.org/about/': 'cache/creativecommons.org.html', \
-                'https://en.support.wordpress.com/': 'cache/support.wordpress.com.html', \
-                'https://en.blog.wordpress.com/': 'cache/blog.wordpress.com.html', \
-                'https://www.deutschland.de/en': 'cache/deutschland.de.en.html', \
-                'https://www.gnu.org/licenses/gpl-3.0.en.html': 'cache/gnu.org.gpl.html', \
-                'https://opensource.org/': 'cache/opensource.org.html', \
-                'https://www.austria.info/': 'cache/austria.info.html', \
+'http://blog.python.org/2016/12/python-360-is-now-available.html': 'blog.python.org.html', \
+'https://example.com': 'example.com.html', \
+'https://github.com/adbar/htmldate': 'github.com.html', \
+'https://creativecommons.org/about/': 'creativecommons.org.html', \
+'https://en.support.wordpress.com/': 'support.wordpress.com.html', \
+'https://en.blog.wordpress.com/': 'blog.wordpress.com.html', \
+'https://www.deutschland.de/en': 'deutschland.de.en.html', \
+'https://www.gnu.org/licenses/gpl-3.0.en.html': 'gnu.org.gpl.html', \
+'https://opensource.org/': 'opensource.org.html', \
+'https://www.austria.info/': 'austria.info.html', \
+'https://www.portal.uni-koeln.de/9015.html?&L=1&tx_news_pi1%5Bnews%5D=4621&tx_news_pi1%5Bcontroller%5D=News&tx_news_pi1%5Baction%5D=detail&cHash=7bc78dfe3712855026fc717c2ea8e0d3': 'uni-koeln.de.ocean.html', \
+'https://www.intel.com/content/www/us/en/legal/terms-of-use.html': 'intel.com.tos.html', \
+'http://www.greenpeace.org/international/en/campaigns/forests/asia-pacific/': 'greenpeace.org.forests.html', \
+  'https://www.amnesty.org/en/what-we-do/corporate-accountability/': 'amnesty.org.corporate.html', \
+'http://www.medef.com/en/content/alternative-dispute-resolution-for-antitrust-damages': 'medef.fr.dispute.html', \
+'https://www.rosneft.com/business/Upstream/Licensing/': 'rosneft.com.licensing.html', \
+'https://www.creativecommons.at/faircoin-hackathon': 'creativecommons.at.faircoin.html', \
+'https://pixabay.com/en/service/terms/': 'pixabay.com.tos.html', \
+'https://futurezone.at/digital-life/wie-creativecommons-richtig-genutzt-wird/24.600.504': 'futurezone.at.cc.html', \
+'https://500px.com/photo/26034451/spring-in-china-by-alexey-kruglov': '500px.com.spring.html', \
+'https://www.eff.org/files/annual-report/2015/index.html': 'eff.org.2015.html', \
+'http://unexpecteduser.blogspot.de/2011/': 'unexpecteduser.2011.html', \
+# '': '', \
 }
 
 TEST_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -32,7 +45,7 @@ TEST_DIR = os.path.abspath(os.path.dirname(__file__))
 
 def load_mock_page(url):
     '''load mock page from samples'''
-    with open(os.path.join(TEST_DIR, MOCK_PAGES[url]), 'r') as inputf:
+    with open(os.path.join(TEST_DIR, 'cache', MOCK_PAGES[url]), 'r') as inputf:
         htmlstring = inputf.read()
     return htmlstring
 
@@ -40,31 +53,46 @@ def load_mock_page(url):
 def test_no_date():
     '''this page should not return any date'''
     assert htmldate.find_date(load_mock_page('https://example.com')) is None
-    # assert htmldate.find_date(load_mock_page('https://en.support.wordpress.com/')) is None
+    assert htmldate.find_date(load_mock_page('https://www.intel.com/content/www/us/en/legal/terms-of-use.html')) is None
+    # safe search
+    assert htmldate.find_date(load_mock_page('https://en.support.wordpress.com/'), False) is None
 
 
 def test_exact_date():
     '''these pages should return an exact date'''
     # meta in header
     assert htmldate.find_date(load_mock_page('http://blog.python.org/2016/12/python-360-is-now-available.html')) == '2016-12-23'
+    assert htmldate.find_date(load_mock_page('https://500px.com/photo/26034451/spring-in-china-by-alexey-kruglov')) == '2013-02-16'
+    # meta in document body
+    assert htmldate.find_date(load_mock_page('https://futurezone.at/digital-life/wie-creativecommons-richtig-genutzt-wird/24.600.504')) == '2013-08-09'
     # in document body
     assert htmldate.find_date(load_mock_page('https://github.com/adbar/htmldate')) == '2017-08-25'
     assert htmldate.find_date(load_mock_page('https://en.blog.wordpress.com/')) == '2017-08-30'
     assert htmldate.find_date(load_mock_page('https://www.gnu.org/licenses/gpl-3.0.en.html')) == '2016-11-18'
     assert htmldate.find_date(load_mock_page('https://opensource.org/')) == '2017-09-05'
     assert htmldate.find_date(load_mock_page('https://www.austria.info/')) == '2017-09-07'
+    assert htmldate.find_date(load_mock_page('https://www.portal.uni-koeln.de/9015.html?&L=1&tx_news_pi1%5Bnews%5D=4621&tx_news_pi1%5Bcontroller%5D=News&tx_news_pi1%5Baction%5D=detail&cHash=7bc78dfe3712855026fc717c2ea8e0d3')) == '2017-07-12'
+    assert htmldate.find_date(load_mock_page('https://www.eff.org/files/annual-report/2015/index.html')) == '2016-05-04'
+    assert htmldate.find_date(load_mock_page('http://unexpecteduser.blogspot.de/2011/')) == '2011-03-30'
+    assert htmldate.find_date(load_mock_page('https://www.rosneft.com/business/Upstream/Licensing/')) == '2017-02-27' # most probably 2014-12-31, found in text
 
 
 def test_approximate_date():
     '''this page should return an approximate date'''
     assert htmldate.find_date(load_mock_page('https://creativecommons.org/about/')) == '2017-08-11' # or '2017-08-03'
     assert htmldate.find_date(load_mock_page('https://www.deutschland.de/en')) == '2017-08-01' # or?
+    assert htmldate.find_date(load_mock_page('http://www.greenpeace.org/international/en/campaigns/forests/asia-pacific/')) == '2017-07-01' # actually "28 April, 2017"
+    assert htmldate.find_date(load_mock_page('https://www.amnesty.org/en/what-we-do/corporate-accountability/')) == '2017-07-01'
+    assert htmldate.find_date(load_mock_page('http://www.medef.com/en/content/alternative-dispute-resolution-for-antitrust-damages')) == '2017-07-01' # actually 2017-09-01
+    assert htmldate.find_date(load_mock_page('https://www.creativecommons.at/faircoin-hackathon')) == '2016-12-15' # actually 2017-07-24
+    assert htmldate.find_date(load_mock_page('https://pixabay.com/en/service/terms/')) == '2017-07-01' # actually 2017-08-09
 
 
 def test_date_validator():
     '''test internal date validation'''
     assert htmldate.date_validator('2016-01-01') is True
     assert htmldate.date_validator('1998-08-08') is True
+    assert htmldate.date_validator('2001-12-31') is True
     assert htmldate.date_validator('1992-07-30') is False
     assert htmldate.date_validator('1901-13-98') is False
     assert htmldate.date_validator('202-01') is False
@@ -107,6 +135,11 @@ def test_search_pattern():
     assert htmldate.search_pattern('It happened in 2002.', pattern, catch, yearpat) is not None
 
 
+def test_search_html():
+    '''test pattern search in HTML'''
+    assert htmldate.search_page(load_mock_page('https://www.portal.uni-koeln.de/9015.html?&L=1&tx_news_pi1%5Bnews%5D=4621&tx_news_pi1%5Bcontroller%5D=News&tx_news_pi1%5Baction%5D=detail&cHash=7bc78dfe3712855026fc717c2ea8e0d3')) == '2017-07-12'
+
+
 def test_cli():
     '''test the command-line interface'''
     pass
@@ -122,6 +155,7 @@ if __name__ == '__main__':
     test_no_date()
     test_exact_date()
     test_approximate_date()
+    test_search_html()
 
     # cli
     # test_cli() ## TODO
