30cca1a58ce9a4f86c487c274b70b3cc8b5f6b71
Adrien Barbaresi <barbaresi@bbaw.de>
2020-01-02 17:37:23 +0100

    setup updated

--- setup.py
+++ setup.py
@@ -1,44 +1,57 @@
-#!/usr/bin/env python
 """
 Seamlessly extract the date of web pages based on header or body.
 http://github.com/adbar/htmldate
 """
 
-import os
-
+# workaround for open() with encoding=''
 from codecs import open
-from setuptools import setup # find_packages,
+
+from os import path
+from setuptools import setup
 
 
-here = os.path.abspath(os.path.dirname(__file__))
+here = path.abspath(path.dirname(__file__))
 packages = ['htmldate']
 
 
+# some problems with installation solved this way
+extras = {
+    'all': [
+        'ciso8601 >= 2.1.1',
+        'dateparser >= 0.7.2', # 0.5.0 could be faster
+        'regex >= 2019.08.19',
+        ],
+}
+
+
 def readme():
-    with open(os.path.join(here, 'README.rst'), 'r', 'utf-8') as readmefile:
+    with open(path.join(here, 'README.rst'), 'r', 'utf-8') as readmefile:
         return readmefile.read()
 
 setup(
     name='htmldate',
-    version='0.5.7',
+    version='0.6.0',
     description='Find the creation date of web pages using a combination of tree traversal, common structural patterns, text-based heuristics and robust date extraction.',
     long_description=readme(),
     classifiers=[
         # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 5 - Production/Stable',
         #'Development Status :: 6 - Mature',
+        'Environment :: Console',
         'Intended Audience :: Developers',
         'Intended Audience :: Information Technology',
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Scientific/Engineering',
+        'Topic :: Internet :: WWW/HTTP',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Text Processing :: Linguistic',
         'Topic :: Text Processing :: Markup :: HTML',
@@ -52,12 +65,11 @@ setup(
     include_package_data=True,
     python_requires='>=3.5',
     install_requires=[
-        'ciso8601 >= 2.1.1',
-        'dateparser >= 0.7.2', # 0.5.0 could be faster
-        'lxml >= 4.4.1', # > 4.3.4 not compatible with Python 3.4
-        'regex >= 2019.08.19',
+        'dateutil' >= '2.8.1',
+        'lxml >= 4.4.1',
         'requests >= 2.22.0',
     ],
+    extras_require=extras,
     entry_points = {
         'console_scripts': ['htmldate=htmldate.cli:main'],
     },
