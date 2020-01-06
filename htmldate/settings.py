c583442ce69bdff175d986c759e6b7adb26c6f99
Adrien Barbaresi <barbaresi@bbaw.de>
2020-01-06 16:34:32 +0100

    + lxml cleaner in settings

--- htmldate/settings.py
+++ htmldate/settings.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+# pylint:disable-msg=E0611
 """
 Listing a series of settings that are applied module-wide.
 """
@@ -8,6 +7,8 @@ Listing a series of settings that are applied module-wide.
 ## under GNU GPL v3 license
 
 import datetime
+
+from lxml.html.clean import Cleaner
 
 # dateparser module
 try:
@@ -34,3 +35,20 @@ MAX_YEAR = datetime.date.today().year
 
 # try dateutil parser
 DEFAULT_PARSER_PARAMS = {'dayfirst': True, 'fuzzy': False}
+
+# HTML_CLEANER config # http://lxml.de/api/lxml.html.clean.Cleaner-class.html
+HTML_CLEANER = Cleaner()
+HTML_CLEANER.comments = False
+HTML_CLEANER.embedded = True
+HTML_CLEANER.forms = False
+HTML_CLEANER.frames = True
+HTML_CLEANER.javascript = True
+HTML_CLEANER.links = False
+HTML_CLEANER.meta = False
+HTML_CLEANER.page_structure = True
+HTML_CLEANER.processing_instructions = True
+HTML_CLEANER.remove_unknown_tags = False
+HTML_CLEANER.safe_attrs_only = False
+HTML_CLEANER.scripts = False
+HTML_CLEANER.style = True
+HTML_CLEANER.kill_tags = ['audio', 'canvas', 'label', 'map', 'math', 'object', 'picture', 'rdf', 'svg', 'video'] # 'embed', 'figure', 'img', 'table'
