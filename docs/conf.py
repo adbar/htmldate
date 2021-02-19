1d8e0a03bc7293826af2a513bde93c9b957128af
Adrien Barbaresi <barbaresi@bbaw.de>
2021-02-19 17:25:53 +0100

    docs: analytics + remove warning

--- docs/conf.py
+++ docs/conf.py
@@ -78,6 +78,7 @@ html_theme_options = {
     "github_repo": "htmldate",
     "github_banner": True,
     "show_related": False,
+    "analytics_id": "G-5BS735G6BB",
 #    "note_bg": "#FFF59C",
 }
 
@@ -85,7 +86,7 @@ html_theme_options = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
 
 
 intersphinx_mapping = {
