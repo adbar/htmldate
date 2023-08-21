407e1a8545d36871134a44d64b4b162e306debea
Adrien Barbaresi <barbaresi@bbaw.de>
2023-08-21 14:51:18 +0200

    fix mypy for Python 3.6

--- htmldate/__init__.py
+++ htmldate/__init__.py
@@ -14,7 +14,7 @@ import logging
 from datetime import datetime
 
 try:
-    datetime.fromisoformat
+    datetime.fromisoformat  # type: ignore[attr-defined]
 except AttributeError:  # Python 3.6
     from backports.datetime_fromisoformat import MonkeyPatch  # type: ignore
 
