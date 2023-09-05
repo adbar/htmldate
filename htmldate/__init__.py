1917a3920323de77b7be64935df7bdf29ff6473f
Adrien Barbaresi <barbaresi@bbaw.de>
2023-09-05 15:56:16 +0200

    bump version

--- htmldate/__init__.py
+++ htmldate/__init__.py
@@ -7,7 +7,7 @@ __title__ = "htmldate"
 __author__ = "Adrien Barbaresi"
 __license__ = "GNU GPL v3"
 __copyright__ = "Copyright 2017-2023, Adrien Barbaresi"
-__version__ = "1.5.0"
+__version__ = "1.5.1"
 
 
 import logging
