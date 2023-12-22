69310fa52bce70be863e9f76ccf431cfc767aa3b
Adam Hupp <142939708+adamh-oai@users.noreply.github.com>
2023-12-22 02:27:10 -0800

    MacOS fix: change lxml setup (#114)
    * Unpin lxml
    
    Per https://github.com/adbar/trafilatura/issues/449, see if unpinning
    lxml works on MacOS.
    
    * specify versions and add platform test
    
    * further specify, add tests
    
    * reduce number of tests
    
    ---------
    
    Co-authored-by: Adrien Barbaresi <adbar@users.noreply.github.com>
    Co-authored-by: Adrien Barbaresi <barbaresi@bbaw.de>

--- setup.py
+++ setup.py
@@ -118,8 +118,9 @@ setup(
         "charset_normalizer >= 3.0.1; python_version < '3.7'",
         "charset_normalizer >= 3.3.2; python_version >= '3.7'",
         "dateparser >= 1.1.2",  # 1.1.3+ slower
-        "lxml >= 4.9.3 ; platform_system != 'Darwin'",
-        "lxml == 4.9.2 ; platform_system == 'Darwin'",
+        # see tests on Github Actions
+        "lxml == 4.9.2 ; platform_system == 'Darwin' and python_version <= '3.8'",
+        "lxml >= 4.9.4 ; platform_system != 'Darwin' or python_version > '3.8'",
         "python-dateutil >= 2.8.2",
         "urllib3 >= 1.26, < 2; python_version < '3.7'",
         "urllib3 >= 1.26, < 3; python_version >= '3.7'",
