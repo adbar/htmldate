f6709a2e990bdbad3b840b90de40910b50bf14f6
Adrien Barbaresi <barbaresi@bbaw.de>
2024-06-11 18:03:05 +0200

    maintenance: focus on modern Python versions

--- setup.py
+++ setup.py
@@ -80,7 +80,6 @@ setup(
         "Operating System :: POSIX :: Linux",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
@@ -113,18 +112,15 @@ setup(
     license="Apache-2.0",
     packages=["htmldate"],
     include_package_data=True,
-    python_requires=">=3.6",
+    python_requires=">=3.8",
     install_requires=[
-        "backports-datetime-fromisoformat; python_version < '3.7'",
-        "charset_normalizer >= 3.0.1; python_version < '3.7'",
-        "charset_normalizer >= 3.3.2; python_version >= '3.7'",
+        "charset_normalizer >= 3.3.2",
         "dateparser >= 1.1.2",  # 1.1.3+ slower
         # see tests on Github Actions
         "lxml == 4.9.2 ; platform_system == 'Darwin' and python_version <= '3.8'",
         "lxml >= 5.2.2, < 6 ; platform_system != 'Darwin' or python_version > '3.8'",
         "python-dateutil >= 2.8.2",
-        "urllib3 >= 1.26, < 2; python_version < '3.7'",
-        "urllib3 >= 1.26, < 3; python_version >= '3.7'",
+        "urllib3 >= 1.26, < 3",
     ],
     extras_require=extras,
     entry_points={
